8061139deb6bac31cf79cd875a9da22ea25a181a
Daria Borisenko <dashapim@gmail.com>
2024-11-11 16:54:36 -0500

    Added constructor into Program class

---
+++ Practice/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Person David = new Person();  //creating object from class Person
+            David.Print();    //name: Dan, age: 35
+        }
+
+        class Person
+        {
+            public string name;
+            public int age;
+
+            public Person()
+            {
+                Console.WriteLine("Creation of object Person");
+                name = "David";
+                age = 35;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"Name: {name} Age: {age}");
+            }
+        }
+    }
+}
