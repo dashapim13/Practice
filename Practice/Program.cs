01057a5dee5fe5f415aafcf2d310ba8429ff8225
Daria Borisenko <dashapim@gmail.com>
2024-11-13 10:54:52 -0500

    Deleted code

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -7,26 +7,9 @@
 
 
 
-            Person David = new Person();  //creating object from class Person
-            David.Print();    //name: Dan, age: 35
-        }
 
-        class Person
-        {
-            public string name;
-            public int age;
 
-            public Person()
-            {
-                Console.WriteLine("Creation of object Person");
-                name = "David";
-                age = 35;
-            }
-
-            public void Print()
-            {
-                Console.WriteLine($"Name: {name} Age: {age}");
-            }
+             }
         }
     }
 }
