f0a943fc2dc1a7a57595fedbdee92653576d93be
Daria Borisenko <dashapim@gmail.com>
2024-11-14 20:38:52 -0500

    creating book object

---
+++ Practice/Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Practice;
+
+
+
+
+public class Book
+{
+	public string title;
+	public string author;
+	public int pages;
+}
---
+++ Practice/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+
+
+
+
+
+
+        }
+    }
+}
