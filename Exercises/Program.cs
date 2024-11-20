f71915f45fd8c2f2f2d62c6a731c7fd6d9cdff88
Daria Borisenko <dashapim@gmail.com>
2024-11-19 21:01:48 -0500

    Two Sum, array, return indices

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -45,45 +45,40 @@
             int[] nums = { 2, 7, 11, 15 };
             int target = 9;
             int[] result = TwoSum(nums, target);
-            if (result.Length == 0)
-            {
-                Console.WriteLine("No two sum solution exists");
-            }
-            else
-            {
-                Console.WriteLine($"Indices: {result[0]}, {result[1]}");
-            }
+
+            Console.WriteLine($"Indices: {result[0]}, {result[1]}");
+
         }
         public static int[] TwoSum(int[] nums, int target)
         {
             //Dictionary to store number and its index
-            Dictionary<int, int> numMap = new Dictionary<int, int>();
+            Dictionary<int, int> numToIndex = new Dictionary<int, int>();
 
             for (int i = 0; i < nums.Length; i++)
             {
-                //Calculate the complement
+                //Find the complement
                 int complement = target - nums[i];
 
                 //Check if the complement exists in the dictionary
-                if (numMap.ContainsKey(complement))
+                if (numToIndex.ContainsKey(complement))
                 {
                     //Return the indices of the two numbers
-                    return new int[] { numMap[complement], i };
+                    return new int[] { numToIndex[complement], i };
                 }
-                //Add the current number and its index to the dictionary
-                if (!numMap.ContainsKey(numMap[i]))
-                {
-                    numMap[nums[i]] = i;
+                //Store the current number and its index to the dictionary
 
-                }
-                //Return an empty array if no solution is found
-                return new int[0];
+                numToIndex[nums[i]] = i;
             }
+
+            throw new ArgumentException("No solution found");
+
+
 
 
         }
     }
 }
+
 
 
 
